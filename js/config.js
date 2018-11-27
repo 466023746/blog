05d527a6c0e2851eaa66706862108a8e1e70b306
娄涛 <loutao@corp.netease.com>
2018-11-27 14:52:33 +0800

    feat: 修改分支

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var config = {
   github: {
     username: '466023746',
     repo: 'cms.js-starter',
-    branch: 'master',
+    branch: 'gh-pages',
     host: 'https://api.github.com',
     // Use prefix option if your site is located in a subdirectory.
     // prefix: 'subdirectory',
