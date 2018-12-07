27c870c5a545ed0adcfe32d583d69272eb4135b3
娄涛 <loutao@corp.netease.com>
2018-12-07 15:49:08 +0800

    feat: add sw

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
 self.importScripts('./node_modules/sw-toolbox/sw-toolbox.js');
 
 self.toolbox.precache([
-    '/images/touch/icon-48.png',
-    '/images/touch/icon-72.png',
-    '/images/touch/icon-96.png',
-    '/images/touch/icon-144.png',
-    '/images/touch/icon-168.png',
-    '/images/touch/icon-192.png',
+    './images/touch/icon-48.png',
+    './images/touch/icon-72.png',
+    './images/touch/icon-96.png',
+    './images/touch/icon-144.png',
+    './images/touch/icon-168.png',
+    './images/touch/icon-192.png',
 ]);
 self.toolbox.router.get('/*', toolbox.networkFirst);
